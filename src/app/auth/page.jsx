50be52914010eb5a9403a154885278fe902242f9
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-10-27 15:47:26 +0900

    fix: Auth 페이지 이미지 삭제
    fix: Auth 페이지 이미지 삭제

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
 
-import logoImage from "/public/images/logoImage.jpg";
-
 import AuthForm from "./auth-form";
 
 export default function AuthPage() {
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center gap-4">
-      <Image src={logoImage} width={300} alt="로고 이미지" />
       <div className="w-full">
         <AuthForm />
       </div>
