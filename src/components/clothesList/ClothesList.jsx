0961110fc23c5544e5d21a1941123c863e4c9b04
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-11-01 22:02:15 +0900

    fix: 높이 수정
    fix: 높이 수정

--- src/components/clothesList/ClothesList.jsx
+++ src/components/clothesList/ClothesList.jsx
@@ -5,7 +5,7 @@ import pupRain from "/public/images/clothes/pupRain.png";
 
 export default function ClothesList() {
   return (
-    <div className="w-full flex flex-col justify-center items-center pt-8">
+    <div className="w-full flex flex-col justify-center items-center pt-4">
       <Image src={catFall} height={320} alt="옷차림 이미지" />
     </div>
   );
