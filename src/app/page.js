5ae9d17eb5c0729c56b99546a709dfcade0f6f14
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-11-02 09:52:09 +0900

    fix: 서식 수정
    fix: 서식 수정

--- src/app/page.js
+++ src/app/page.js
@@ -7,21 +7,21 @@ import ClothesList from "@/components/clothesList/ClothesList";
 import UltravioletCard from "@/components/card/UltravioletCard";
 
 export default function Home() {
-  useEffect(() => {
-    // 페이지가 마운트되면 터치 스크롤 이벤트를 막습니다.
-    const handleTouchMove = (e) => {
-      e.preventDefault();
-    };
+  // useEffect(() => {
+  //   // 페이지가 마운트되면 터치 스크롤 이벤트를 막습니다.
+  //   const handleTouchMove = (e) => {
+  //     e.preventDefault();
+  //   };
 
-    document.addEventListener("touchmove", handleTouchMove, {
-      passive: false,
-    });
+  //   document.addEventListener("touchmove", handleTouchMove, {
+  //     passive: false,
+  //   });
 
-    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
-    return () => {
-      document.removeEventListener("touchmove", handleTouchMove);
-    };
-  }, []);
+  //   // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
+  //   return () => {
+  //     document.removeEventListener("touchmove", handleTouchMove);
+  //   };
+  // }, []);
   return (
     <div className="w-full min-h-screen flex flex-col justify-start items-center gap-3 relative h-screen overflow-hidden">
       <Header />
